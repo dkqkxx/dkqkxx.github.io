6282467e832b58859f89e67e1701f68f670e9816
dkqkxx <dkqkxx@gmail.com>
2024-04-23 00:36:52 +0800

    machine learning

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Hello World",'url':"/2017/01/01/hello-world.html"},{'title':"互联网与算法",'url':"/2024/02/15/internet-and-algorithm.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Hello World",'url':"/2017/01/01/hello-world.html"}]};
